783a87535a12b981b450d27c011d12f7974ae2f7
Martin Moy <martinmoy1991@gmail.com>
2019-10-07 04:42:36 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,13 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+]
 def display_board(board)
    puts " #{board[0]} | #{board[1]} | #{board[2]} "
    puts "-----------"
@@ -11,21 +21,10 @@ def input_to_index(input)
 end
 
 
-def move(board, index, value = "O" || "X")
+def move(board, index, value)
   board[index] = value
   return board
 end
-
-WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8],
-]
 
 
 def position_taken?(board, index)
@@ -38,4 +37,67 @@ def valid_move?(board, index)
   else
     return false
   end
+end
+
+def turn_count(board)
+  turn_count = 0
+  board.each do |value|
+    if value == "X" || value == "O"
+      turn_count += 1
+    end
+  end
+  return turn_count
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X": "O"
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combinations|
+    position_1 = combinations[0]
+    position_2 = combinations[1]
+    position_3 = combinations[2]
+    if board[position_1] == "X" && board[position_2] == "X" && board[position_3]  == "X"
+      return combinations
+    elsif board[position_1] == "O" && board[position_2] == "O" && board[position_3] == "O"
+      return combinations
+    end
+  end
+end
+
+def full?(board)
+  board.all? do |positions|
+    positions == "X" || positions == "O"
+  end
+end
+
+def draw?(board)
+ !won?(board) && full?(board) ? true:false
+end
+
+def over?(board)
+  won?(board) || draw?(board) ? true:false
+end
+
+def winner(board)
+  if win_combination = won?(board)
+    board[win_combination.first]
+  else
+    return nil
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input=gets.strip
+  index=input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, value = "X" || value = "O")
+  else
+    return turn(board)
+  end
+end
+
+def play(board)
 end
