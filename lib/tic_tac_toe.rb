54be2cb851ab6b640af182efe875776c4ba77d9b
Martin Moy <martinmoy1991@gmail.com>
2020-10-03 08:02:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,7 +28,8 @@ end
 
 
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " " || board[index] == "")
+  #!(board[index].nil? || board[index] == " " || board[index] == "")
+  (board[index] == "X" || board[index] == "O")
 end
 
 def valid_move?(board, index)
